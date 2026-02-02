ba224fd2eeb91c53a9d2bb3d87d533ff80c0416d
Zain-kuben <zaasa003@osloskolen.no>
2026-02-02 17:43:37 +0100

    Jeg har lagt til 4 faller som faller, med tilfeldig x verdi og y

--- spill.py
+++ spill.py
@@ -1,6 +1,9 @@
 import pygame as pg
+import random
 
 pg.init()
+
+font = pg.font.SysFont("Arial", 20)
 
 class Kurv:
     def __init__(self, x_pos:int, y_pos:int, bredde:int, høyde:int, fart:int, vindu, farge:tuple):
@@ -40,6 +43,7 @@ class Kurv:
         elif self._x_pos + self._bredde >= self._vindu.get_width():
             self._x_pos = self._vindu.get_width() - self._bredde
 
+
 class Ball:
     def __init__(self, x_pos:int, y_pos:int, radius:int, fart:float, vindu, farge:tuple):
         self._x_pos = x_pos
@@ -50,13 +54,21 @@ class Ball:
         self._farge = farge
 
     def tegne_ball(self):
-        pg.draw.circle(self._vindu, self._farge, (self._x_pos, self._y_pos), self._radius)
+        pg.draw.circle(self._vindu, self._farge, (int(self._x_pos), int(self._y_pos)), self._radius)
     
     def flytt(self):
         self._y_pos += self._fart
 
+    def kollisjon(self, kurv: Kurv):
+        if self._x_pos + self._radius > kurv._x_pos and self._y_pos + self._radius > kurv._y_pos and self._x_pos - self._radius < kurv._x_pos + kurv._bredde and self._y_pos - self._radius <= kurv._y_pos + kurv._høyde:
+            return True
+    
+    def restart(self):
+        self._y_pos = -20
+        self._x_pos = random.randint(20, self._vindu.get_width() - 20)
+        self._fart = 0.2
 
-
+             
 
 VINDU_BREDDE = 500
 VINDU_HOYDE = 500
@@ -64,9 +76,17 @@ vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 pg.display.set_caption("Bruk piltaster for å bevege")
 
 kurv = Kurv(225, 450, 50, 20, 0.5, vindu, (255, 0, 0))
-ball = Ball(250, 0, 15, 0.4, vindu, (0, 0, 255))
+
+ball1 = Ball(random.randint(20, VINDU_BREDDE - 20), -20, 20, 0.2, vindu, (0, 0, 0))
+ball2 = Ball(random.randint(20, VINDU_BREDDE - 20), -80, 20, 0.2, vindu, (0, 0, 0))
+ball3 = Ball(random.randint(20, VINDU_BREDDE - 20), -140, 20, 0.2, vindu, (0, 0, 0))
+ball4 = Ball(random.randint(20, VINDU_BREDDE - 20), -200, 20, 0.2, vindu, (0, 0, 0))
+
+baller = [ball1, ball2, ball3, ball4]
+
 
 fortsett = True
+gameover = False
 
 while fortsett:
     for event in pg.event.get():
@@ -75,12 +95,31 @@ while fortsett:
 
     trykkede_taster = pg.key.get_pressed()
 
+
+   
     kurv.rettning(trykkede_taster)
     kurv.flytt()
     kurv.grenser()
 
+    for ball in baller:
+            ball.flytt()
+
+    for ball in baller:
+        if ball.kollisjon(kurv):
+            gameover = True
+            for ball in baller:
+                ball._fart = 0
+
+
     vindu.fill((255, 255, 255))
     kurv.tegne()
+    for ball in baller:
+        ball.tegne_ball()
+
+    if gameover:
+        bilde = font.render("Game over! Trykk SPACE", True, (50, 50, 50))
+        vindu.blit(bilde, (150, 20))
+
     pg.display.flip()
 
 pg.quit()
