a14f49402ced7271b771963c0dfa35da2122e00d
Zain-kuben <zaasa003@osloskolen.no>
2026-02-03 10:33:25 +0100

    Laget ferdig reset knap, manger bare 3 liv og at ballene skal komme opp igjen

--- spill.py
+++ spill.py
@@ -45,45 +45,57 @@ class Kurv:
 
 
 class Ball:
-    def __init__(self, x_pos:int, y_pos:int, radius:int, fart:float, vindu, farge:tuple):
+    def __init__(self, x_pos:int, y_pos:int, bredde:int, høyde:int, fart:float, vindu, farge:tuple):
         self._x_pos = x_pos
         self._y_pos = y_pos
-        self._radius = radius
+        self._bredde = bredde
+        self._høyde = høyde
         self._fart = fart
         self._vindu = vindu
         self._farge = farge
 
     def tegne_ball(self):
-        pg.draw.circle(self._vindu, self._farge, (int(self._x_pos), int(self._y_pos)), self._radius)
+        pg.draw.rect(
+            self._vindu,
+            self._farge,
+            (self._x_pos, self._y_pos, self._bredde, self._høyde)
+        )
     
     def flytt(self):
         self._y_pos += self._fart
 
     def kollisjon(self, kurv: Kurv):
-        if self._x_pos + self._radius > kurv._x_pos and self._y_pos + self._radius > kurv._y_pos and self._x_pos - self._radius < kurv._x_pos + kurv._bredde and self._y_pos - self._radius <= kurv._y_pos + kurv._høyde:
+        if (
+            self._x_pos + self._bredde > kurv._x_pos and
+            self._y_pos + self._høyde > kurv._y_pos and
+            self._x_pos < kurv._x_pos + kurv._bredde and
+            self._y_pos <= kurv._y_pos + kurv._høyde
+        ):
             return True
-    
+        
     def restart(self):
-        self._y_pos = -20
-        self._x_pos = random.randint(20, self._vindu.get_width() - 20)
+        self._y_pos = random.randint(-400, -20)
+        self._x_pos = random.randint(20, self._vindu.get_width() - self._bredde)
         self._fart = 0.2
 
-             
 
 VINDU_BREDDE = 500
 VINDU_HOYDE = 500
 vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 pg.display.set_caption("Bruk piltaster for å bevege")
 
-kurv = Kurv(225, 450, 50, 20, 0.5, vindu, (255, 0, 0))
+kurv = Kurv(225, 450, 50, 20, 0.5, vindu, (255, 80, 80))
 
-ball1 = Ball(random.randint(20, VINDU_BREDDE - 20), -20, 20, 0.2, vindu, (0, 0, 0))
-ball2 = Ball(random.randint(20, VINDU_BREDDE - 20), -80, 20, 0.2, vindu, (0, 0, 0))
-ball3 = Ball(random.randint(20, VINDU_BREDDE - 20), -140, 20, 0.2, vindu, (0, 0, 0))
-ball4 = Ball(random.randint(20, VINDU_BREDDE - 20), -200, 20, 0.2, vindu, (0, 0, 0))
+ball1 = Ball(random.randint(20, VINDU_BREDDE - 20), -20, 20, 20, 0.2, vindu, (0, 0, 0))
+ball2 = Ball(random.randint(20, VINDU_BREDDE - 20), -80, 20, 20, 0.2, vindu, (0, 0, 0))
+ball3 = Ball(random.randint(20, VINDU_BREDDE - 20), -140, 20, 20, 0.2, vindu, (0, 0, 0))
+ball4 = Ball(random.randint(20, VINDU_BREDDE - 20), -200, 20, 20, 0.2, vindu, (0, 0, 0))
+ball5 = Ball(random.randint(20, VINDU_BREDDE - 20), -260, 20, 20, 0.2, vindu, (0, 0, 0))
+ball6 = Ball(random.randint(20, VINDU_BREDDE - 20), -320, 20, 20, 0.2, vindu, (0, 0, 0))
+ball7 = Ball(random.randint(20, VINDU_BREDDE - 20), -380, 20, 20, 0.2, vindu, (0, 0, 0))
+ball8 = Ball(random.randint(20, VINDU_BREDDE - 20), -440, 20, 20, 0.2, vindu, (0, 0, 0))
 
-baller = [ball1, ball2, ball3, ball4]
-
+baller = [ball1, ball2, ball3, ball4, ball5, ball6, ball7, ball8]
 
 fortsett = True
 gameover = False
@@ -95,29 +107,32 @@ while fortsett:
 
     trykkede_taster = pg.key.get_pressed()
 
-
-   
     kurv.rettning(trykkede_taster)
     kurv.flytt()
     kurv.grenser()
 
+    if trykkede_taster[pg.K_SPACE]:
+        for ball in baller:
+            ball.restart()
+        gameover = False
+
     for ball in baller:
-            ball.flytt()
+        ball.flytt()
 
     for ball in baller:
         if ball.kollisjon(kurv):
             gameover = True
-            for ball in baller:
-                ball._fart = 0
+            for b in baller:
+                b._fart = 0
 
 
-    vindu.fill((255, 255, 255))
+    vindu.fill((30, 30, 40))
     kurv.tegne()
     for ball in baller:
         ball.tegne_ball()
 
     if gameover:
-        bilde = font.render("Game over! Trykk SPACE", True, (50, 50, 50))
+        bilde = font.render("Game over! Trykk SPACE", True, (230, 230, 230))
         vindu.blit(bilde, (150, 20))
 
     pg.display.flip()
